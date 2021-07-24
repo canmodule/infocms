895f89b816cb0b2c0a6f8f913cf1ab68096dc213
acanhost <acanstudio@canliang.wang>
2021-07-24 09:39:10 +0800

    develop main

--- app/Resources/Pet.php
+++ app/Resources/Pet.php
@@ -2,8 +2,6 @@
 
 namespace ModuleInfocms\Resources;
 
-use Framework\Baseapp\Resources\AbstractResource;
-
 class Pet extends AbstractResource
 {
 }
--- app/Resources/PetPetsort.php
+++ app/Resources/PetPetsort.php
@@ -2,8 +2,6 @@
 
 namespace ModuleInfocms\Resources;
 
-use Framework\Baseapp\Resources\AbstractResource;
-
 class PetPetsort extends AbstractResource
 {
 }
