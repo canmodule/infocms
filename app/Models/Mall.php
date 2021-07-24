773644db064a65e8466151d7a57846b6e584546b
acanhost <acanstudio@canliang.wang>
2021-07-24 10:35:42 +0800

    develop main

---
+++ app/Models/Mall.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ModuleInfocms\Models;
+
+class Mall extends AbstractModel
+{
+    protected $table = 'mall';
+    //protected $guarded = ['id'];
+
+}
