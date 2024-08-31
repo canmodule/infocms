af900359ac3ef3ddab6a51000ef68b379b1a6940
acanhost <acanstudio@canliang.wang>
2024-08-31 11:19:44 +0800

    develop infocms

--- app/Services/HistoryDataTrait.php
+++ app/Services/HistoryDataTrait.php
@@ -7,6 +7,144 @@ use Swoolecan\Foundation\Helpers\DatetimeTool;
 
 trait HistoryDataTrait
 {
+    public function getFigureDetail($figureCode)
+    {
+        $info = $this->getModelObj('group')->where(['code' => $figureCode])->first();
+        if (empty($info)) {
+            return $this->resource->throwException(400, '信息不存在-' . $figureCode);
+        }
+        $figure = $this->getModelObj('culture-figure')->where(['code' => $figureCode])->first();
+        $titles = $figure->getFtitleDatas();
+        $birthData = $figure->getBirthDeath();
+        //print_r($birthData);
+        //print_r($titles);
+
+        $detail = [
+            'user' => [
+                'user_id' => 1,
+                'avatar' => $figure ? $figure->photoUrl : 'https://q.zhuige.com/wp-content/uploads/2022/07/1.png',
+                'nickname' => $info['name'],
+                'titles' => $titles,
+                'birthData' => $birthData,
+                'brief' => $figure ? $figure['description'] : '',
+                'cover' => $this->getRandBackground(),
+                'is_me' => 0,
+                'is_follow' => 0,
+                'is_fans' => 0,
+                'delete_topic' => 0,
+                'delete_vote' => 0,
+                'delete_business_card' => 0,
+                'delete_idle_shop' => 0,
+            ],
+            'rec_ad' => [
+                'title' => '圈内推荐',
+                'items' => [
+                    [
+                        'title' => '本代码下载',
+                        'badge' => 'Hot',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/marjan-blan-marjanblan-k06giuMSd6s-unsplash-750x536-1.jpg',
+                        'link' => 'https://www.zhuige.com/product/zg.html',
+                        'price' => '￥0.00',
+                    ],
+                    [
+                        'title' => '安装/帮助文档',
+                        'badge' => '',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/velizar-ivanov-9bFLTsaP_xo-unsplash-350x250-1.jpg',
+                        'link' => 'https://www.zhuige.com/docs.html',
+                        'price' => '→ 看看',
+                    ],
+                    [
+                        'title' => '追格小程序（全模块）',
+                        'badge' => '推荐',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/manny-moreno-Wxq7U4jaPfM-unsplash-350x250-1.jpg',
+                        'link' => 'https://www.zhuige.com/product/zgxcx.html',
+                        'price' => '￥6800',
+                    ],
+                    [
+                        'title' => '插件模块市场',
+                        'badge' => '',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/tt.jpg',
+                        'link' => 'https://www.zhuige.com/product.html?cat=24',
+                        'price' => '￥19.9起',
+                    ],
+                ],
+            ],
+            'btn_message' => 1,
+            'btn_promotion' => 1,
+            'tab_idle' => 1,
+            'tabs' => $this->figureMenus(),
+        ];
+        return $detail;
+    }
+
+    public function getGroupDetail($groupCode)
+    {
+        $info = $this->getModelObj('group')->where(['code' => $groupCode])->first();
+        if (empty($info)) {
+            return $this->resource->throwException(400, '信息不存在-' . $groupCode);
+        }
+
+        $detail = [
+            'users' => [],
+            'id' => $info['id'],
+            'name' => $info['name'],
+            'user_count' => rand(100, 10000),
+            'post_count' => rand(100, 5000),
+            'is_follow' => rand(0, 1),
+            'is_owner' => rand(0, 1),
+            'logo' => $info['logo'] ?: 'http://39.106.102.45/resource/knowledge/history/%E5%8F%B2%E5%89%8D%E6%96%87%E6%98%8E/%E7%9F%B3%E5%99%A8%E6%97%B6%E4%BB%A3-%E9%99%B6%E9%B8%9F.png',
+            'background' => $this->getRandBackground(),
+            'notice' => '追格小程序是一个积木式小程序搭建框架，基于Uniapp+WordPress开发，代码免费开源不加密且支持二开，安装追格相应扩展模块，简单配置即可快速搭建不限于圈子社区、知识付费、活动报名、资讯等类型小程序。',
+            'ad_link' => [
+                'title' => '追格小程序安装文档、常见问题',
+                'link' => 'https://www.zhuige.com/docs/cat/39.html',
+            ],
+            'location' => [
+                'marker' => '',
+                'address' => '',
+                'longitude' => '',
+                'latitude' => '',
+            ],
+            'ad_custom' => [
+            ],
+            'groupMenus' => $this->groupMenus(),
+            'ad_imgs' => [
+                'title' => '圈内推荐',
+                'items' => [
+                    [
+                        'title' => '本代码下载',
+                        'badge' => 'Hot',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/marjan-blan-marjanblan-k06giuMSd6s-unsplash-750x536-1.jpg',
+                        'link' => 'https://www.zhuige.com/product/zg.html',
+                        'price' => '￥0.00',
+                    ],
+                    [
+                        'title' => '安装/帮助文档',
+                        'badge' => '',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/velizar-ivanov-9bFLTsaP_xo-unsplash-350x250-1.jpg',
+                        'link' => 'https://www.zhuige.com/docs.html',
+                        'price' => '→ 看看',
+                    ],
+                    [
+                        'title' => '追格小程序（全模块）',
+                        'badge' => '推荐',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/manny-moreno-Wxq7U4jaPfM-unsplash-350x250-1.jpg',
+                        'link' => 'https://www.zhuige.com/product/zgxcx.html',
+                        'price' => '￥6800',
+                    ],
+                    [
+                        'title' => '插件模块市场',
+                        'badge' => '',
+                        'image' => 'https://q.zhuige.com/wp-content/uploads/2022/09/tt.jpg',
+                        'link' => 'https://www.zhuige.com/product.html?cat=24',
+                        'price' => '￥19.9起',
+                    ],
+                ],
+            ],
+        ];
+        return $detail;
+    }
+
     public function getSubjectInfos($subjectSort)
     {
         $infos = $this->getModelObj('subject')->where(['subject_sort' => $subjectSort])->orderBy('orderlist', 'desc')->get();;
@@ -27,5 +165,34 @@ trait HistoryDataTrait
             $results[] = $data;
         }
         return $results;
+    }
+
+    public function groupMenus()
+    {
+        $datas = [
+            ['title' => '图表', 'route' => '/pages/knowledge/graphic'],
+            ['title' => '文章', 'route' => '/pages/knowledge/'],
+            ['title' => '人物', 'route' => '/pages/knowledge/'],
+            ['title' => '事件', 'route' => '/pages/knowledge/'],
+        ];
+        return $datas;
+    }
+
+    public function getRandBackground()
+    {
+        $num = rand(1, 6);
+        return 'http://39.106.102.45/resource/knowledge/common/%E8%83%8C%E6%99%AF%E5%9B%BE-' . $num . '.jpg';
+    }
+
+    public function figureMenus()
+    {
+        $datas = [
+            ['id' => 'biography', 'title' => '生平'],
+            ['id' => 'graphic', 'title' => '图说'],
+            ['id' => 'work', 'title' => '著作'],
+            ['id' => 'article', 'title' => '文章'],
+            ['id' => 'mimiarticle', 'title' => '帖子'],
+        ];
+        return $datas;
     }
 }
