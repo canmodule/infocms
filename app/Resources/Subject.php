2d37b8d1c5eb99e27baf740a676f03fcf6312b37
acanhost <acanstudio@canliang.wang>
2024-10-07 14:28:29 +0800

    develop dealed

--- app/Resources/Subject.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ModuleInfocms\Resources;
-
-class Subject extends AbstractResource
-{
-}
--- app/Resources/SubjectCollection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ModuleInfocms\Resources;
-
-class SubjectCollection extends AbstractCollection
-{
-}
